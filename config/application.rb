9be0a4d9fa82fe44b5bf1bdb57895b67ff874260
Chorkpop <apedley@gmail.com>
2013-06-09 16:17:57 -0500

    removing extra assets

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module Antinote
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.assets.paths << Rails.root.join("vendor", "fonts")
+    # config.assets.paths << Rails.root.join("vendor", "fonts")
     # require 'pdfkit'
     # config.middleware.use PDFKit::Middleware
 
